7c7c790706b20a51e3f9e8823679e74c1eba3003
Reysmer Valle <reysmerwvr@gmail.com>
2019-03-31 20:54:55 -0500

    Callbacks | Files | Streams | Buffers | Pipes

---
+++ F11_Pipes/Starter/app.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var zlib = require('zlib');
+
+var readable = fs.createReadStream(__dirname + '/greet.txt', { encoding: 'utf8', highWaterMark: 16 * 1024 });
+
+var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
+
+var compressed = fs.createWriteStream(__dirname + '/greet.txt.gz');
+
+var gzip = zlib.createGzip();
+
+readable.pipe(writable);
+
+readable.pipe(gzip).pipe(compressed);
+// readable.on('data', function(chunk) {
+// 	console.log(chunk);
+// 	writable.write(chunk);
+// });
