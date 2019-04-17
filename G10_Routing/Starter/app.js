2cc37c58e324b0c661ad90d532ef02c2c97a61f3
Reysmer Valle <reysmerwvr@gmail.com>
2019-04-16 23:10:26 -0500

    Outputting HTML | Streams and Performance | Routing

--- G10_Routing/Starter/app.js
+++ G10_Routing/Starter/app.js
@@ -3,11 +3,18 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
 
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  var obj = {
-    firstname: 'John',
-    lastname: 'Doe'
-  };
-  res.end(JSON.stringify(obj));
-
-}).listen(8080, '127.0.0.1');
+  if(req.url === '/') {
+    fs.createReadStream(__dirname + '/index.htm').pipe
+    (res);
+  } else if (req.url === '/api') {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    var obj = {
+      firstname: 'John',
+      lastname: 'Doe'
+    };
+    res.end(JSON.stringify(obj));
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+}).listen(1337, '127.0.0.1');
